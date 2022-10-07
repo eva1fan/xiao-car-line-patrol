0e1ef6430badba72e57e8e3d5ab3152706a9219f
eva1fan <107357853+eva1fan@users.noreply.github.com>
2022-10-07 19:43:26 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,32 @@
-basic.showString("Hello")
-music.playMelody("E B C5 A B G A F ", 120)
-music.playMelody("A F E F D G E F ", 120)
-music.playMelody("G C E C D G D C ", 120)
+let 等待指令时长 = 0
+let 直行时长 = 0
 basic.showLeds(`
-    . . # . .
-    . . # . .
-    . # # # .
-    # . # . #
-    . # # . .
+    . . # . #
+    . . # . #
+    . . # . #
+    . . . . #
+    . . # . #
     `)
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    # # . # #
-    # # . # .
-    . . # . .
-    `)
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    . . # . .
-    . . . . .
-    . . # . .
-    `)
-basic.showIcon(IconNames.Sword)
-basic.forever(function () {
-    Tinybit.Music_Car(Tinybit.enMusic.dadadum)
-    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 70)
-    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 70)
+loops.everyInterval(200, function () {
+    if (Tinybit.Voice_Sensor() > 100) {
+        while (直行时长 < 5) {
+            Tinybit.Music_Car(Tinybit.enMusic.entertainer)
+            basic.showNumber(直行时长)
+            if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+                Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 50)
+                直行时长 += 1
+            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+                Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 50)
+                直行时长 = 0
+            } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+                Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 50)
+                直行时长 = 0
+            } else {
+                Tinybit.CarCtrl(Tinybit.CarState.Car_SpinLeft)
+            }
+        }
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
     } else {
-        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 70)
+        等待指令时长 += 1
     }
 })
